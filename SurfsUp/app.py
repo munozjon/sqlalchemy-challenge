2e587afe713be13a395aca9f64ccfa3e0bddf8af
munozjon <munozjon98@gmail.com>
2024-05-07 23:08:30 -0400

    Began work on the Flask app, imported dependencies and initialized database connections

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,58 @@
 # Import the dependencies.
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
+station = Base.classes.station
 
+measurement = Base.classes.measurement
 
 # Create our session (link) from Python to the DB
-
+# session = Session(bind=engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def homepage():
+    """Listing available api routes."""
+    return (
+        f"Here are all available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
