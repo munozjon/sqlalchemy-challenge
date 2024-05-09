852401e14db41548adef4c05c0ca7a3f8e590605
munozjon <munozjon98@gmail.com>
2024-05-08 20:19:25 -0400

    Added two routes to app.py Flask

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,5 +1,7 @@
 # Import the dependencies.
 import numpy as np
+import datetime as dt
+import pandas as pd
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -26,7 +28,7 @@ station = Base.classes.station
 measurement = Base.classes.measurement
 
 # Create our session (link) from Python to the DB
-# session = Session(bind=engine)
+session = Session(bind=engine)
 
 #################################################
 # Flask Setup
@@ -51,6 +53,40 @@ def homepage():
         f"/api/v1.0/<start>/<end>"
     )
 
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    recent_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
+    recent_dt = dt.datetime.strptime(recent_date.date, "%Y-%m-%d")
+    earliest_month = recent_dt - dt.timedelta(days=366)
+
+    last_months = session.query(measurement.date, measurement.prcp).\
+        filter(measurement.date >= earliest_month).all()
+    
+    session.close()
+
+    latest_prcp = []
+    for date, prcp in last_months:
+        prcp_dict = {}
+        prcp_dict[date] = prcp
+        latest_prcp.append(prcp_dict)
+    
+    return jsonify(latest_prcp)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    stations = session.query(station.name).all()
+
+    session.close()
+
+    stations_list = list(np.ravel(stations))
+
+    return jsonify(stations_list)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    
 
 
 if __name__ == '__main__':
